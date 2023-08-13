f113d3c5e1b324d69f48100407a9ab4794d80ff4
NassimBouNasr <nassimbounasr02@gmail.com>
2023-08-13 15:39:59 +0300

    initial commit

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import Layout from "../components/layout"
+
+export default function PageNotFound() {
+    return (
+        <Layout>
+            <h1>page not found</h1>
+        </Layout>
+    )
+}
